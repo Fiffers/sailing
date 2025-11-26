b02b72fb8a952997048d4c8432e85343981a2a2f
Max Weber <abex@abex.sh>
2025-11-26 14:46:40 -0700

    LuffOverlay: use objchange state

--- src/main/java/com/duckblade/osrs/sailing/features/facilities/LuffOverlay.java
+++ src/main/java/com/duckblade/osrs/sailing/features/facilities/LuffOverlay.java
@@ -12,11 +12,8 @@ import java.awt.Shape;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameObject;
-import net.runelite.api.events.ChatMessage;
-import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
@@ -28,17 +25,9 @@ public class LuffOverlay
 	extends Overlay
 	implements PluginLifecycleComponent
 {
-
-	private static final String CHAT_LUFF_AVAILABILE = "You feel a gust of wind.";
-	private static final String CHAT_LUFF_PERFORMED = "You trim the sails, catching the wind for a burst of speed!";
-	private static final String CHAT_LUFF_STORED = "You trim the sails, catching the wind and storing a wind mote in your helm.";
-	private static final String CHAT_LUFF_ENDED = "The wind dies down and your sails with it.";
-
 	private final Client client;
 	private final SailingConfig config;
 	private final BoatTracker boatTracker;
-
-	private boolean needLuff = false;
 
 	@Inject
 	public LuffOverlay(Client client, SailingConfig config, BoatTracker boatTracker)
@@ -57,37 +46,28 @@ public class LuffOverlay
 		return config.highlightTrimmableSails();
 	}
 
-	@Subscribe
-	public void onChatMessage(ChatMessage e)
-	{
-		if (!SailingUtil.isSailing(client) ||
-			(e.getType() != ChatMessageType.GAMEMESSAGE && e.getType() != ChatMessageType.SPAM))
-		{
-			return;
-		}
-
-		String msg = e.getMessage();
-		if (CHAT_LUFF_AVAILABILE.equals(msg))
-		{
-			needLuff = true;
-		}
-		else if (CHAT_LUFF_PERFORMED.equals(msg) || CHAT_LUFF_STORED.equals(msg) || CHAT_LUFF_ENDED.equals(msg))
-		{
-			needLuff = false;
-		}
-	}
-
 	@Override
 	public Dimension render(Graphics2D g)
 	{
-		if (!needLuff || !SailingUtil.isSailing(client) || !config.highlightTrimmableSails())
+		if (!SailingUtil.isSailing(client) || !config.highlightTrimmableSails())
 		{
 			return null;
 		}
 
 		Boat boat = boatTracker.getBoat();
 		GameObject sail = boat != null ? boat.getSail() : null;
-		Shape convextHull = sail != null ? sail.getConvexHull() : null;
+
+		if (sail == null)
+		{
+			return null;
+		}
+
+		if (!sail.isOpShown(0))
+		{
+			return null;
+		}
+
+		Shape convextHull =sail.getConvexHull();
 		if (convextHull != null)
 		{
 			OverlayUtil.renderPolygon(g, convextHull, Color.green);
